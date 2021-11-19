ce1113a0c065a02edfc372f066e9f80d1b7bf5ff
darksworm <9987548+darksworm@users.noreply.github.com>
2021-11-19 11:51:49 +0200

    scheduler workaround test

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,17 @@
 set -e
 
 if [ "${1}" = "run_worker" ]; then
-  node dist/schedule.js
+  TIME_SLEPT=0
+  while true; do
+   TIME_SLEPT=$(expr $TIME_SLEPT + 1)
+   if [ "$TIME_SLEPT" -eq "5" ]; then
+     node dist/jobs/update_events.js || echo "UPDATE_SETLISTS FAIL" && echo "UPDATE_SETLISTS SUCCESS"
+     TIME_SLEPT=0
+   else
+     node dist/jobs/update_setlists.js || echo "UPDATE_EVENTS FAIL" && echo "UPDATE_EVENTS SUCCESS"
+   fi
+   sleep 1
+  done
 elif [ "${1}" = "run_bot" ]; then
   node dist/index.js
 elif [ "${1}" = "migrate" ]; then
