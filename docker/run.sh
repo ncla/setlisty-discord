70cfd71751ade9bb2c7b541b17d458cc18b5f05b
darksworm <9987548+darksworm@users.noreply.github.com>
2021-11-23 15:12:52 +0200

    WIP: tests pipeline

--- docker/run.sh
+++ docker/run.sh
@@ -10,7 +10,7 @@ elif [ "${1}" = "migrate" ]; then
 elif [ "${1}" = "register" ]; then
   node dist/deploy_commands.js
 elif [ "${1}" = "test" ]; then
-  yarn jest
+  yarn run test
 else
   echo expected run_worker, run_bot, migrate or register, got "${1}" instead
 fi
